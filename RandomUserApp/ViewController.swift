f132d7b58c603c20bc92879d93df2821ddbcceca
Danil Anikin <mart4r.clearlist@gmail.com>
2025-09-24 16:06:57 +1000

    Initial Commit

---
+++ RandomUserApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUserApp
+//
+//  Created by Данил Аникин on 24/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
