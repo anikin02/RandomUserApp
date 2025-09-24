85bb42e2510c8c990fbaaf75e7e47ed9369df6b6
Danil Anikin <mart4r.clearlist@gmail.com>
2025-09-24 23:13:00 +1000

    Added func getRandomUser in API

--- RandomUserApp/API/APIManger.swift
+++ RandomUserApp/API/APIManger.swift
@@ -5,8 +5,30 @@
 //  Created by Данил Аникин on 24/09/2025.
 //
 
+import Foundation
+
 class APIManger {
   static let shared = APIManger()
   private init() {}
   
+  func getRandomUser(completion: @escaping (User) -> Void) {
+      let urlString = "https://randomuser.me/api/"
+      guard let url = URL(string: urlString) else { return }
+      
+      let session = URLSession(configuration: .default)
+      let task = session.dataTask(with: url, completionHandler: { (data, response, error) in
+        if error != nil {
+          print(error!.localizedDescription)
+        } else if let data = data {
+          do {
+            let decoder = JSONDecoder()
+            let response = try decoder.decode(UserResponse.self, from: data)
+            completion(response.results.first!)
+          } catch {
+            print(error.localizedDescription)
+          }
+        }
+      })
+      task.resume()
+    }
 }
