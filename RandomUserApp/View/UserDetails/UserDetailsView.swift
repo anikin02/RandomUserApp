28d0f10bd956a94bde60b7aac560b21bc2c96856
Danil Anikin <mart4r.clearlist@gmail.com>
2025-09-26 15:16:12 +1000

    Created UserDetailsView

---
+++ RandomUserApp/View/UserDetails/UserDetailsView.swift
@@ -0,0 +1,15 @@
+//
+//  UserDetailsView.swift
+//  RandomUserApp
+//
+//  Created by Данил Аникин on 26/09/2025.
+//
+
+import SwiftUI
+
+struct UserDetailsView: View {
+  let user: User
+  var body: some View {
+    Text("Hello, world!")
+  }
+}
